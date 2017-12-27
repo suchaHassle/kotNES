041cedc285c63a4fedb6ebe8bd0f8b369168fbd6
Jason Pham <me@jasonpham.ca>
2017-12-26 19:41:50 -0500

    Add setter for PPU memory

--- src/PpuMemory.kt
+++ src/PpuMemory.kt
@@ -9,6 +9,17 @@ class PpuMemory {
         2 -> ppuFlags.PPUSTATUS and 0xFF
         5 -> ppuFlags._lastWrittenRegister and 0xFF
         6 -> ppuFlags._lastWrittenRegister and 0xFF
-        else -> IllegalAccessError("This address is illegal $address")
+        else -> IllegalAccessError("This address is illegal, $address")
+    }
+
+    operator fun set(address: Int, value: Int) {
+        ppuFlags._lastWrittenRegister = value and 0xFF
+        when (address) {
+            0 -> ppuFlags.PPUCTRL = value
+            1 -> ppuFlags.PPUMASK = value
+            2 -> return
+            5 -> ppuFlags.PPUSCROLL = value
+            6 -> ppuFlags.PPUADDR = value
+        }
     }
 }
