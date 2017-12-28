11b2794353281a3d088d20585f995dfa295e6d1b
Jason Pham <me@jasonpham.ca>
2017-12-27 22:36:59 -0500

    Implement fallback rendering

---
+++ src/Renderers/FallbackRenderManager.java
@@ -0,0 +1,49 @@
+package Renderers;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.peer.ComponentPeer;
+
+/**
+ * This render manager uses whatever sketchy unknown renderer used by Java.
+ *
+ * @author Tudor
+ * @author Quantum
+ */
+public class FallbackRenderManager implements IRenderManager
+{
+    /**
+     * Field to store the peer.
+     */
+    private final ComponentPeer peer;
+
+    /**
+     * Constructs a fallback renderer for the {@link java.awt.Panel} whose peer is given.
+     *
+     * @param peer the peer of the {@link java.awt.Panel} for which the renderer is constructed.
+     */
+    public FallbackRenderManager(ComponentPeer peer)
+    {
+        this.peer = peer;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Graphics2D getGraphics()
+    {
+        // Return the default peer.
+        Toolkit.getDefaultToolkit().sync();
+        return (Graphics2D) peer.getGraphics();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void update()
+    {
+        // There is nothing needed to do when switching.
+    }
+}
