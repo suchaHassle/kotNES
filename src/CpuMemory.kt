f17285558c519c76fc90b9ec13a63ce9a5bba8c9
Jason Pham <me@jasonpham.ca>
2018-01-05 18:06:38 -0500

    Add apu IO registers addresses

--- src/CpuMemory.kt
+++ src/CpuMemory.kt
@@ -14,15 +14,15 @@ class CpuMemory(var emulator: Emulator) {
     var cartridge = emulator.cartridge
     var mapper: NROM = NROM(cartridge)
 
-    class NotImplementedException(override var message: String) : Exception()
-
+    private var apuIoRegisters = IntArray(0x20)
     private val internalRam = IntArray(0x800)
 
     operator fun get(address: Int): Int = when (address) {
         in 0x0000..0x1FFF -> internalRam[address % 0x800] and 0xFF
         in 0x2000..0x3FFF -> emulator.ppu.ppuMemory.readRegister(address)
+        in 0x4000..0x401F -> apuIoRegisters[address-0x4000]
         in 0x4020..0xFFFF -> mapper.read(address) and 0xFF
-        else -> throw NotImplementedException("Only internal RAM Address right now: " + address)
+        else -> throw IndexOutOfBoundsException("$address is out of bounds")
     }
 
     fun readWord(address: Int): Int = (get(address + 1) shl 8) or get(address)
@@ -36,8 +36,9 @@ class CpuMemory(var emulator: Emulator) {
         when (address) {
             in 0x0000..0x1FFF -> internalRam[address % 0x800] = value
             in 0x2000..0x3FFF -> emulator.ppu.ppuMemory.writeRegister(address, value)
+            in 0x4000..0x401F -> apuIoRegisters[address-0x4000] = value
             in 0x4020..0xFFFF -> mapper.write(address, value)
-            else -> throw NotImplementedException("Only internal RAM Address right now")
+            else -> throw IndexOutOfBoundsException("$address is out of bounds")
         }
     }
 }
